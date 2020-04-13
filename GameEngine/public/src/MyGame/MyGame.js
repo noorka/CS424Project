9e5c573d21b3b8d4e1d2bb66861983b7f036b3f2
noorka <akroon@luc.edu>
2020-04-13 13:13:25 -0500

    Making duck array with random placed duck.

--- GameEngine/public/src/MyGame/MyGame.js
+++ GameEngine/public/src/MyGame/MyGame.js
@@ -16,7 +16,13 @@ function MyGame() {
 
     // the hero and the support objects
     this.mTarget = null;
-    this.mDuck = null;
+
+    this.mFlock = null;
+    //this.mDuck = null;
+    this.xRange = 20;
+    this.xStart = 10;
+    this.yRange = 10;
+    this.yStart = 55;
 }
 gEngine.Core.inheritPrototype(MyGame, Scene);
 
@@ -41,23 +47,58 @@ MyGame.prototype.initialize = function () {
 
     // Step B: Create the support objects
 
-    // The right minion
-    this.mDuck= new SpriteAnimateRenderable(this.kDuckSprite);
-    this.mDuck.setColor([1, 1, 1, 0]);
-    this.mDuck.getXform().setPosition(26, 56.5);
-    this.mDuck.getXform().setSize(2, 2);
-    this.mDuck.setSpriteSequence(512, 0,     // first element pixel position: top-left 512 is top of image, 0 is left of image
+
+    // This should create a duck sprite with a unique position that can be fed into the array with a loop
+    MyGame.prototype.makeDuck = function (){
+        var chick = new SpriteAnimateRenderable(this.kDuckSprite);
+        var x, y;
+        x = Math.floor(Math.random() * this.xRange + this.xStart);
+        y = Math.floor(Math.random() * this.yRange + this.yStart); 
+        //console.log(x);
+        //console.log(y);
+        //x = 25;
+        //y = 55;
+
+        chick.setColor([1, 1, 1, 0]);
+        chick.getXform().setSize(2, 2);
+        chick.getXform().setPosition(x, y);
+        chick.setSpriteSequence(512, 0,     // first element pixel position: top-left 512 is top of image, 0 is left of image
                                     256, 256,       // widthxheight in pixels
-                                    4,              // number of elements in this sequence
-                                    0);             // horizontal padding in between
-    this.mDuck.setAnimationType(SpriteAnimateRenderable.eAnimationType.eAnimateRight);
-    this.mDuck.setAnimationSpeed(25);
+                                     4,              // number of elements in this sequence
+                                     0);             // horizontal padding in between
+        chick.setAnimationType(SpriteAnimateRenderable.eAnimationType.eAnimateRight);
+        chick.setAnimationSpeed(25);
+        return chick;
+    }
+    MyGame.prototype.makeFlock = function (flockSize){
+        var i = 0;
+        var flock = new Array();
+        for(i; i<flockSize; i++){
+            flock.push(this.makeDuck());
+            console.log("pushing ducks");
+        }
+        console.log("flock made");
+        return flock;
+    }
+
+    // this.mDuck= new SpriteAnimateRenderable(this.kDuckSprite);
+    // this.mDuck.setColor([1, 1, 1, 0]);
+    // this.mDuck.getXform().setPosition(26, 56.5);
+    // this.mDuck.getXform().setSize(2, 2);
+    // this.mDuck.setSpriteSequence(512, 0,     // first element pixel position: top-left 512 is top of image, 0 is left of image
+                                    //256, 256,       // widthxheight in pixels
+                                    //4,              // number of elements in this sequence
+                                    //0);             // horizontal padding in between
+    // this.mDuck.setAnimationType(SpriteAnimateRenderable.eAnimationType.eAnimateRight);
+    //this.mDuck.setAnimationSpeed(25);
                                 // show each element for mAnimSpeed updates
 
+    this.mFlock = this.makeFlock(5);
 
     this.mTarget = new SpriteRenderable(this.kDuckSprite);
     this.mTarget.setColor([1, 1, 1, 0]);
-    this.mTarget.getXform().setPosition(20, 60);
+    this.mTarget.getXform().setPosition(25, 65);
+    // 20 60
     this.mTarget.getXform().setSize(1.5, 1.5);
     this.mTarget.setElementPixelPositions(0, 256, 0, 256);
 };
@@ -65,6 +106,7 @@ MyGame.prototype.initialize = function () {
 // This is the draw function, make sure to setup proper drawing environment, and more
 // importantly, make sure to _NOT_ change any state.
 MyGame.prototype.draw = function () {
+    var i = 0;
     // Step A: clear the canvas
     gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
 
@@ -72,7 +114,11 @@ MyGame.prototype.draw = function () {
     this.mCamera.setupViewProjection();
 
     // Step  C: Draw everything
-    this.mDuck.draw(this.mCamera.getVPMatrix());
+    //this.mFlock.draw(this.mCamera.getVPMatrix());
+    for(i; i <this.mFlock.length; i++){
+        this.mFlock[i].draw(this.mCamera.getVPMatrix());
+    }
+
     this.mTarget.draw(this.mCamera.getVPMatrix());
     // this.mDuck.draw(this.mCamera.getVPMatrix());
 };
@@ -84,6 +130,7 @@ MyGame.prototype.update = function () {
 
     var deltaX = 0.1;
     var deltaY = 0.1;
+    var i = 0;
 
     var xform = this.mTarget.getXform();
     // Support hero movements
@@ -109,6 +156,9 @@ MyGame.prototype.update = function () {
         xform.incYPosBy(-deltaY);
     }
 
-    this.mDuck.updateAnimation();
+    //this.mFlock.updateAnimation();
+    for(i; i <this.mFlock.length; i++){
+        this.mFlock[i].updateAnimation();
+    }
 
 };
