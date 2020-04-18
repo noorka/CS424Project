f90ca0ad0b0a4731af8453807c3402fc3ed1b2ed
Eliora Horst <eliora.horst@gmail.com>
2020-04-18 21:03:58 +0100

    Pond image working

--- GameEngine/public/src/MyGame/MyGame.js
+++ GameEngine/public/src/MyGame/MyGame.js
@@ -3,7 +3,7 @@
 function MyGame() {
     this.kDuckSprite = "assets/Ducks_Bobbing.png";
     this.kSinkingDucks = "assets/Sinking_Ducks.png";
-    this.kBackgroundImage = "assets/background.png";
+    this.kBackgroundImage = "assets/pond.png";
 
     // The camera to view the scene
     this.mCamera = null;
@@ -41,21 +41,20 @@ MyGame.prototype.initialize = function () {
         100,                       // width of camera
         [0, 0, 750, 1334]           // viewport (orgX, orgY, width, height)
     );
-    //this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
+    this.mCamera.setBackgroundColor([0.31, 0.7, 0.29, 1]);
             // sets the background to gray
 
    
     this.mBackground = new SpriteRenderable(this.kBackgroundImage);
     this.mBackground.setColor([1, 1, 1, 0]);
-    this.mBackground.getXform().setPosition(0, 0);
-    this.mBackground.getXform().setSize(750, 1334);
-    this.mBackground.setElementPixelPositions(0, 750, 0, 1334);
+    this.mBackground.getXform().setPosition(50, 50);
+    this.mBackground.getXform().setSize(93.6, 69.9);
+    this.mBackground.setElementPixelPositions(0, 397, 0, 294);
 
     this.mBread = new Bread(this.kDuckSprite);
     this.mDuckHit = new SinkingDuck(this.kSinkingDucks, 35, 45);
     this.mDuckHit.setVisibility(false);
     this.mFlock = new Flock(this.kDuckSprite, 15);
-
 
     this.mMsg = new FontRenderable("Score: 0");
     this.mMsg.setColor([0, 0, 0, 1]);
@@ -71,6 +70,7 @@ MyGame.prototype.draw = function () {
 
     // Step  B: Activate the drawing Camera
     this.mCamera.setupViewProjection();
+    this.mBackground.draw(this.mCamera);
 
     this.mMsg.draw(this.mCamera);
     
@@ -80,10 +80,9 @@ MyGame.prototype.draw = function () {
         this.mFlock[i].draw(this.mCamera);
         console.log("DUCK");
     }*/
+
     this.mDuckHit.draw(this.mCamera);
     this.mBread.draw(this.mCamera);
-    this.mBackground.draw(this.mCamera);
-    
 };
 
 // The update function, updates the application state. Make sure to _NOT_ draw
